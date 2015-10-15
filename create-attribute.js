c5445db275f828803badf09861085706cf73c821
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-10-15 14:10:08 +0500

    Some fixes

--- create-attribute.js
+++ create-attribute.js
@@ -5,50 +5,50 @@ var properties = propConfig.properties;
 var attributeNames = propConfig.attributeNames;
 
 var prefixAttribute = memoizeString(function (name) {
-  return escape(name) + '="';
+	return escape(name) + '="';
 });
 
 module.exports = createAttribute;
 
 /**
- * Create attribute string.
- *
- * @param {String} name The name of the property or attribute
- * @param {*} value The value
- * @param {Boolean} [isAttribute] Denotes whether `name` is an attribute.
- * @return {?String} Attribute string || null if not a valid property or custom attribute.
- */
+* Create attribute string.
+*
+* @param {String} name The name of the property or attribute
+* @param {*} value The value
+* @param {Boolean} [isAttribute] Denotes whether `name` is an attribute.
+* @return {?String} Attribute string || null if not a valid property or custom attribute.
+*/
 
 function createAttribute(name, value, isAttribute) {
-  name = (attributeNames[name] || name).toLowerCase();
-  return prefixAttribute(name) + escape(value) + '"';
+	name = (attributeNames[name] || name).toLowerCase();
+	return prefixAttribute(name) + escape(value) + '"';
 }
 
 /**
- * Should skip false boolean attributes.
- */
+* Should skip false boolean attributes.
+*/
 
 function shouldSkip(name, value) {
-  var attrType = properties[name];
-  return value == null ||
-    (attrType === types.BOOLEAN && !value) ||
-    (attrType === types.OVERLOADED_BOOLEAN && value === false);
+	var attrType = properties[name];
+	return value == null ||
+	(attrType === types.BOOLEAN && !value) ||
+	(attrType === types.OVERLOADED_BOOLEAN && value === false);
 }
 
 /**
- * Memoizes the return value of a function that accepts one string argument.
- *
- * @param {function} callback
- * @return {function}
- */
+* Memoizes the return value of a function that accepts one string argument.
+*
+* @param {function} callback
+* @return {function}
+*/
 
 function memoizeString(callback) {
-  var cache = {};
-  return function(string) {
-    if (cache.hasOwnProperty(string)) {
-      return cache[string];
-    } else {
-      return cache[string] = callback.call(this, string);
-    }
-  };
+	var cache = {};
+	return function(string) {
+		if (cache.hasOwnProperty(string)) {
+			return cache[string];
+		} else {
+			return cache[string] = callback.call(this, string);
+		}
+	};
 }
